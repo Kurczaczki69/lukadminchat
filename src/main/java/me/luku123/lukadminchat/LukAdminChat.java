2029ef34bbd513260f33e94bacc91bada7f9e786
BuildTools <unconfigured@null.spigotmc.org>
2024-07-12 20:19:20 +0200

    basic files

---
+++ src/main/java/me/luku123/lukadminchat/LukAdminChat.java
@@ -0,0 +1,18 @@
+package me.luku123.lukadminchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LukAdminChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("[LukAdminChat] Enabling LukAdminChat v1.0.0");
+        getLogger().info("[LukAdminChat] Plugin LukAdminChat v1.0.0 has been enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("[LukAdminChat] Disabling LukAdminChat v1.0.0");
+        getLogger().info("[LukAdminChat] Plugin LukAdminChat v1.0.0 has been disabled");
+    }
+}
