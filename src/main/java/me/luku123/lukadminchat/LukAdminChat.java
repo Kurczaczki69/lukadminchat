dcbf32febaff800030ed1aac65d718420e1563ce
BuildTools <unconfigured@null.spigotmc.org>
2024-07-13 15:29:04 +0200

    update ChatEvents and LukAdminChat

--- src/main/java/me/luku123/lukadminchat/LukAdminChat.java
+++ src/main/java/me/luku123/lukadminchat/LukAdminChat.java
@@ -6,11 +6,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LukAdminChat extends JavaPlugin {
 
+    private static LukAdminChat plugin;
+
     @Override
     public void onEnable() {
         getLogger().info("[LukAdminChat] Enabling LukAdminChat v1.0.0");
+        plugin = this;
 
         Bukkit.getServer().getPluginManager().registerEvents(new ChatEvents(), this);
+        saveDefaultConfig();
 
         getLogger().info("[LukAdminChat] Plugin LukAdminChat v1.0.0 has been enabled ");
     }
@@ -20,4 +24,9 @@ public final class LukAdminChat extends JavaPlugin {
         getLogger().info("[LukAdminChat] Disabling LukAdminChat v1.0.0");
         getLogger().info("[LukAdminChat] Plugin LukAdminChat v1.0.0 has been disabled");
     }
+
+    public static LukAdminChat getPlugin() {
+        return plugin;
+    }
+
 }
--- src/main/java/me/luku123/lukadminchat/events/ChatEvents.java
+++ src/main/java/me/luku123/lukadminchat/events/ChatEvents.java
@@ -1,7 +1,9 @@
 package me.luku123.lukadminchat.events;
 
+import me.luku123.lukadminchat.LukAdminChat;
 import me.luku123.lukadminchat.utils.ColorCode;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,17 +12,18 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 public class ChatEvents implements Listener {
 
     ColorCode cc = new ColorCode();
+    FileConfiguration cfg = LukAdminChat.getPlugin().getConfig();
 
     @EventHandler
     public void OnChatSend(AsyncPlayerChatEvent e) {
         String msg = e.getMessage();
 
-        if (msg.startsWith("!")) {
-            String newmsg = msg.replace("!", "");
+        if (msg.startsWith(cfg.getString("AdminChatPrefix"))) {
+            String newmsg = msg.replace(cfg.getString("AdminChatPrefix"), "");
 
             for (Player player1 : Bukkit.getOnlinePlayers()) {
                 if (player1.hasPermission("lukadminchat.use")) {
-                    player1.sendMessage( cc.CC("&e(&lADMIN-CHAT&r&e) ") + cc.CC(newmsg));
+                    player1.sendMessage( cc.CC(cfg.getString("AdminChatMsg")) + cc.CC(newmsg));
                     e.setCancelled(true);
                 }
                 else {
